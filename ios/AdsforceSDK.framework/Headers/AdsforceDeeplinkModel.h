37f7f81fdbcb9b6e3ee01cb9b3890c388c4fe30d
samliu <sam.liu@avid.ly>
2019-03-20 16:06:19 +0800

    update ios

---
+++ ios/AdsforceSDK.framework/Headers/AdsforceDeeplinkModel.h
@@ -0,0 +1,16 @@
+//
+//  AdsforceDeeplinkModel.h
+//  AdsforceSDK
+//
+//  Created by liuguojun on 2018/6/1.
+//  Copyright © 2018 Adsforce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdsforceDeeplinkModel : NSObject
+
+@property (nonatomic,copy) NSString *targetUrl;
+@property (nonatomic,copy) NSString *linkArgs;
+
+@end
